fb7f8c64b30fcba500b82a5e22c949da121b33b0
Chris Good <goodca91@gmail.com>
2013-01-13 14:28:24 -0500

    initial commit for the ATDCape project

---
+++ ATDCape/src/ATDCape.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ATDCape.c
+ Author      : Chris Good, Sarah Waller, Logan Bonifer, Yaomin Zha
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hello world"); /* prints hello world */
+	return EXIT_SUCCESS;
+}
